4311e93376b218467e452d2daa371429550b6fb3
godori <gej.okpo@gmail.com>
2019-07-01 03:03:11 +0900

    !

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>🚢 STAGING BRANCH</h1>
+        <h1>🚢 STAGING BRANCH !</h1>
       </header>
     </div>
   );
