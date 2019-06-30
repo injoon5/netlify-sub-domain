6da6e4ddc006b9d242c4b0cd6f8bf9be6662149b
godori <gej.okpo@gmail.com>
2019-07-01 00:02:56 +0900

    change emoji

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>🔧 DEVELOP BRANCH</h1>
+        <h1>⚒ DEVELOP BRANCH</h1>
       </header>
     </div>
   );
